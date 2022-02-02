c27453e71a57f1a7dc89046dc6fa88015f987df3
Andrew Yarmola <andrew.yarmola@gmail.com>
2022-02-02 18:03:12 -0500

    initial plans for pants decomps

---
+++ surface/fenchel_nielsen.py
@@ -0,0 +1,2 @@
+# Fenchel-Nielsen Coordinates
+# Outputs generators for group as well
---
+++ surface/pants_decomposition.py
@@ -0,0 +1,24 @@
+# Implements pants decomposition of surfaces
+import networkx
+
+class PantsDecompositon:
+    def __init__(self):
+        # store a trivalent networkx graph
+        # store boundary vertices
+        # store boundary pairs
+
+    def genus(self):
+        pass
+    
+    
+
+
+def generate_all_pants_decomps(g):
+    # bournady verts
+    # internal verts
+    # conenct in systematic way to generate all
+    # use networkx to get all isomorphism classes
+
+
+
+
